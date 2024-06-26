49e557b19197f82f27f3731d20a9c5f392ec24a8
Szymon Michalak <77302574+Szymon-Michalak@users.noreply.github.com>
2024-06-26 08:56:44 +0200

    Init xcode project

---
+++ PolygonNetwork/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PolygonNetwork
+//
+//  Created by Szymon Michalak on 26/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PolygonNetwork/PolygonNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolygonNetworkApp.swift
+//  PolygonNetwork
+//
+//  Created by Szymon Michalak on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PolygonNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
